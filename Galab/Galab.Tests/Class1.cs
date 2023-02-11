b9bfc95fe38dabe39b9b8a399004afdd763cee77
Piotr Bejger <piotr.bejger@gmail.com>
2023-02-11 21:24:11 +0100

    no message

--- Galab/Galab.Tests/Class1.cs
+++ Galab/Galab.Tests/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using EventStore.Client;
+using Microsoft.Azure.Cosmos;
 using Xunit;
 
 namespace Galab.Tests
@@ -6,10 +7,25 @@ namespace Galab.Tests
     public class Class1
     {
         [Fact]
-        public async Task test()
+        [Trait("Category", "EventStore")]
+        public async Task test_event_store()
         {
             var c = new EventStoreClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
             await c.AppendToStreamAsync("str", StreamState.Any, new List<EventData> { new(Uuid.NewUuid(), "et", new ReadOnlyMemory<byte>()), });
+        }
+
+        [Fact]
+        [Trait("Category", "EventStore")]
+        public async Task test_cosmos_db()
+        {
+            var clientOptions = new CosmosClientOptions
+            {
+                HttpClientFactory = () => new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (_, _, _, _) => true, }),
+                ConnectionMode = ConnectionMode.Gateway,
+            };
+
+            var c = new CosmosClient("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", clientOptions);
+            await c.CreateDatabaseIfNotExistsAsync("testdbgalab");
         }
     }
 }
