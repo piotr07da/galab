24e53c3238df9376cc7d309a242681c402892a0c
Piotr Bejger <piotr.bejger@gmail.com>
2023-02-11 13:24:02 +0100

    no message

--- Galab/Galab.Tests/Class1.cs
+++ Galab/Galab.Tests/Class1.cs
@@ -1,12 +1,15 @@
-﻿using Xunit;
+﻿using EventStore.Client;
+using Xunit;
 
 namespace Galab.Tests
 {
     public class Class1
     {
         [Fact]
-        public void test()
+        public async Task test()
         {
+            var c = new EventStoreClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
+            await c.AppendToStreamAsync("str", StreamState.Any, new List<EventData> { new(Uuid.NewUuid(), "et", new ReadOnlyMemory<byte>()), });
         }
     }
 }
